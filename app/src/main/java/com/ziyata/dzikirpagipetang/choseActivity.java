2eb38fd09b8d87b485293bca33458101bcb4e93e
ilham <ziyatakhsani@gmail.com>
2018-11-23 11:18:07 +0700

    Initial commit

---
+++ app/src/main/java/com/ziyata/dzikirpagipetang/choseActivity.java
@@ -0,0 +1,25 @@
+package com.ziyata.dzikirpagipetang;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class choseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chose);
+    }
+
+    public void dzikirPagi(View view) {
+        Intent pindah = new Intent(choseActivity.this, dzikirPagiActivity.class);
+        startActivity(pindah);
+    }
+
+    public void dzikirPetang(View view) {
+        Intent pindah = new Intent(choseActivity.this, dzikirPetangActivity.class);
+        startActivity(pindah);
+    }
+}
